0e5a072b080e1c9b5ee0b8d8fd96aa4e13d29522
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-09-11 23:42:28 +0530

    feat: Add productModel and userModel

---
+++ src/models/userModel.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "../interfaces/userInterface";
+
+const userSchema: Schema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'customer'],
+        default: 'customer'
+    }
+})
+
+const userModel = mongoose.model<IUser>('User', userSchema);
+
+export default userModel;
