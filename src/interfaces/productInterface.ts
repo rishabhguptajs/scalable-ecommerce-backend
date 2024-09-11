997f8b62cf502e2ae40701ac36036d12a267f6a5
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-09-11 23:42:18 +0530

    feat: Add product and user interfaces

---
+++ src/interfaces/productInterface.ts
@@ -0,0 +1,9 @@
+import { Document } from "mongoose";
+
+export interface IProduct extends Document{
+    name: string,
+    price: number,
+    description: string,
+    category: string,
+    stock: number
+}
